27bfd45c91a35bafb55a5e266d280d21ac10fb3f
Philipekum <filipp.tsekhansky@yandex.ru>
2023-02-14 18:24:47 +0300

    added library to realize search of services and finally reworked CRMRequest

--- CRMRequest.py
+++ CRMRequest.py
@@ -1,4 +1,5 @@
 import requests as r
+from fuzzywuzzy import process
 from config import config
 
 
@@ -7,47 +8,75 @@ class CRMRequest:
         self.__url = config.url.get_secret_value()
         self.__token = config.crm_token.get_secret_value()
 
-        get_service_url: str = f'{self.__url}getServices/{self.__token}'
-        get_employee_url: str = f'{self.__url}getEmployee/{self.__token}'
+        self.service_url = f'{self.__url}getServices/{self.__token}'
+        self.service_response = r.get(url=self.service_url)
+        self.service_data = self.service_response.json()
 
-        # Makes a dict with keys as ['name'] of dictionary in a list and dictionaries as values
-        name_dictator = lambda response: dict(
-            zip(
-                [element['name'] for element in response],
-                response
-            )
-        )
+        self.employee_url = f'{self.__url}getEmployees/{self.__token}'
+        self.employee_response = r.get(url=self.employee_url)
 
-        service_response = r.get(url=get_service_url, timeout=5).json()
+    def get_categories(self) -> list:
+        data = self.service_data
+        categories = []
+        for service_dict in data:
+            categories.append(service_dict.get('name'))
 
-        self.services = name_dictator(service_response)
+        return categories
 
-        # employee_response = r.get(url=get_employee_url, timeout=5).json()
-        #
-        # self.employees = name_dictator(employee_response)
+    def get_services(self, category: str = None) -> list:
+        if category is None:
+            # return all services in each category
+            pass
 
-    # def get_service_names(self, category: str) -> list:
-    #     if category not in self.services:
-    #         raise ValueError
+        data = [x for x in self.service_data if x.get('name') == category][0]
+        self.service_data = data.get('services')
+        services = []
+
+        for service in data.get('services'):
+            services.append(service.get('name'))
+
+        return services
+
+    def get_employees(self, service_name: str = None) -> dict:
+        if service_name is None:
+            # return all employees
+            pass
+
+        employee_data: list = self.employee_response.json()
+        picked_service_data: dict = [x for x in self.service_data if x.get('name') == service_name][0]
+        service_id: int = picked_service_data.get('id')
+        names = []
+        ids = []
+
+        for emp in employee_data:
+            if service_id in emp['serviceEmployeesIds']:
+                name = emp['lastname'].title() + ' ' + emp['firstname'].title() + ' ' + emp['patronymic'].title()
+                names.append(name)
+                ids.append(emp['id'])
+
+        res = dict(zip(ids, names))
+
+        return res
+
+    def get_dates(self, doctor: str) -> list:
+        pass
+
+    def get_times(self, doctor: str, date: str) -> list:
+        pass
+
+    # def search(self, request: str) -> list:
+    #     coincidences = process.extract(request, self.services.keys())
+    #     res = []
     #
-    #     for i in self.services:
-    #         if i['name'] == category:
-    #
-    #             for j in i['services']:
-    #                 res.append(j['name'])
-    #
-    #             break
+    #     for i in coincidences:
+    #         if i[1] >= 75:
+    #             res.append(i[0])
     #
     #     return res
 
-    # def get_employee_by_service(self, service: str) -> list:
-    #     res = []
-    #     if service not in self.services[]
-
-
-from config import config
 
 if __name__ == '__main__':
     req = CRMRequest()
-    # services = req.get_command('getServices').json()
-    print(req.services.keys())
+    print(req.get_categories())
+    print(req.get_services('Хирургия'))
+    print(req.get_employees('Удаление экзостоза'))
