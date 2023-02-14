b5d86f2af937479960d981cd739445da10286b6b
Philipekum <filipp.tsekhansky@yandex.ru>
2023-02-14 19:52:46 +0300

    get_dates completed

--- CRMRequest.py
+++ CRMRequest.py
@@ -15,51 +15,98 @@ class CRMRequest:
         self.employee_url = f'{self.__url}getEmployees/{self.__token}'
         self.employee_response = r.get(url=self.employee_url)
 
+        self.dates_url = f'{self.__url}getScheduleCache/{self.__token}'
+
+        self.post_data = {
+            "client": {
+                "firstname": "Иван",
+                "surname": "Иванов",
+                "patronymic": "",
+                "telephone": "+7(904)243-23-43",
+                "proccesingOfPersonalData": True
+            },
+            "visitItems": [
+                {
+                    "employeeId": "2",
+                    "serviceId": 163,
+                    "startTime": "14:00"
+                }
+            ],
+            "employee": {
+                "id": 0
+            },
+            "visit": {
+                "date": "2022-01-28",
+                "comment": "Позвоните мне"
+            }
+        }
+
     def get_categories(self) -> list:
         data = self.service_data
         categories = []
-        for service_dict in data:
-            categories.append(service_dict.get('name'))
+        for service_category in data:
+            categories.append(service_category.get("name"))
 
         return categories
 
     def get_services(self, category: str = None) -> list:
+
+        # Return all services in default
         if category is None:
-            # return all services in each category
-            pass
+            services = []
+            for service_category in self.service_response.json():
+                for service in service_category["services"]:
+                    services.append(service["name"])
 
-        data = [x for x in self.service_data if x.get('name') == category][0]
-        self.service_data = data.get('services')
+            return services
+
+        data = [x for x in self.service_data if x.get("name") == category][0]
         services = []
+        self.service_data = data.get("services")
 
-        for service in data.get('services'):
-            services.append(service.get('name'))
+        for service in data.get("services"):
+            services.append(service.get("name"))
 
         return services
 
-    def get_employees(self, service_name: str = None) -> dict:
-        if service_name is None:
-            # return all employees
-            pass
-
+    def get_employees(self, service_name: str = None) -> list:
         employee_data: list = self.employee_response.json()
-        picked_service_data: dict = [x for x in self.service_data if x.get('name') == service_name][0]
-        service_id: int = picked_service_data.get('id')
         names = []
-        ids = []
+
+        # Return all employees in default
+        if service_name is None:
+            for emp in employee_data:
+                name = emp["lastname"].title() + " " + emp["firstname"].title() + " " + emp["patronymic"].title()
+                names.append(name)
+
+            return names
+
+        picked_service_data: dict = [x for x in self.service_data if x.get("name") == service_name][0]
+        service_id: int = picked_service_data.get("id")
 
         for emp in employee_data:
-            if service_id in emp['serviceEmployeesIds']:
-                name = emp['lastname'].title() + ' ' + emp['firstname'].title() + ' ' + emp['patronymic'].title()
+            if service_id in emp["serviceEmployeesIds"]:
+                name = emp["lastname"].title() + " " + emp["firstname"].title() + " " + emp["patronymic"].title()
                 names.append(name)
-                ids.append(emp['id'])
 
-        res = dict(zip(ids, names))
+        return names
 
-        return res
+    def get_dates(self, employee: str) -> list:
+        data = self.employee_response.json()
+        name = employee.split(' ')
+        employee_id = None
+        for emp in data:
+            if emp["lastname"].lower() == name[0].lower() and emp["firstname"].lower() == name[1].lower() and \
+                    name[2].lower() == emp["patronymic"].lower():
+                employee_id = str(emp["id"])
 
-    def get_dates(self, doctor: str) -> list:
-        pass
+        date_data = r.get(url=self.dates_url).json()
+        dates = []
+        for date in date_data:
+            if employee_id in date:
+                dates.append(date)
+
+        return dates
 
     def get_times(self, doctor: str, date: str) -> list:
         pass
@@ -77,6 +124,7 @@ class CRMRequest:
 
 if __name__ == '__main__':
     req = CRMRequest()
-    print(req.get_categories())
-    print(req.get_services('Хирургия'))
-    print(req.get_employees('Удаление экзостоза'))
+    # print(req.get_categories())
+    # print(req.get_services('Хирургия'))
+    # print(req.get_employees('Удаление экзостоза'))
+    print(req.get_dates('Османова Фарида Ибрагимовна'))
