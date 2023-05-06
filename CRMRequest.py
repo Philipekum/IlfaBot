96ecaed3b18889ef1b2864e64cd33efc3aea551b
filip <filipp.tsekhansky@yandex.ru>
2023-05-06 06:52:21 +0300

    every func optimized, get_times only left

--- CRMRequest.py
+++ CRMRequest.py
@@ -1,10 +1,8 @@
-import pydantic
 import requests as r
-
 import DataModel
 from config import config
-from DataModel import Category, Employee
-from pydantic import parse_obj_as
+from DataModel import Category, Employee, Date
+from pydantic import parse_obj_as, ValidationError
 from datetime import datetime, timedelta
 
 
@@ -18,17 +16,8 @@ class CRMRequest:
         self.dates_url = f'{self.__url}getScheduleCache/{self.__token}'
         self.time_url = f'{self.__url}getSchedule/{self.__token}'
 
-        try:
-            self.service_data = self._get_parsed_data(service_url, Category)
-
-        except (ConnectionError, ValueError) as e:
-            print(e)
-
-        try:
-            self.employee_data = self._get_parsed_data(employee_url, Employee)
-
-        except (ConnectionError, ValueError) as e:
-            print(e)
+        self.service_data = self._get_parsed_data(service_url, Category)
+        self.employee_data = self._get_parsed_data(employee_url, Employee)
 
         # self.post_data = {
         #     "client": {
@@ -55,32 +44,38 @@ class CRMRequest:
         # }
 
     @staticmethod
-    def _get_parsed_data(url: str, model: DataModel.Any) -> list:
+    def _get_parsed_data(url: str, model: DataModel.Any) -> list | dict:
         try:
             response = r.get(url)
             response.raise_for_status()
             data = response.json()
 
-            for element in data:
-                model.validate(element)
+            if isinstance(data, list):
+                for element in data:
+                    model.validate(element)
 
-            return parse_obj_as(list[model], data)
+                return parse_obj_as(list[model], data)
+
+            elif isinstance(data, dict):
+                model.validate(data)
+
+                return parse_obj_as(model, data)
 
         except r.exceptions.RequestException:
-            raise ConnectionError(f'Ошибка при получении данных: {url}')
+            raise ConnectionError(f'Connection to CRM not worked: {url}')
 
-        except pydantic.ValidationError:
-            raise ValueError(f'Ошибка при обработке данных: {url}')
+        except ValidationError:
+            raise ValueError(f'Data processing Error: {model}')
 
-    def _get_employee_id(self, employee_name: str) -> str:
+    def _get_employee_id(self, employee_name: str) -> int:
         """Gets id of employee as str"""
         employee_name = employee_name.casefold().split(" ")
         employee_name = [employee_name.pop(0), employee_name.pop(0), " ".join(employee_name)]
         for employee in self.employee_data:
             if list(map(str.casefold, [employee.lastname, employee.firstname, employee.patronymic])) == employee_name:
-                return str(employee.id)
+                return employee.id
 
-        raise ValueError(f'Employee {employee_name} not found')
+        raise ValueError(f'Employee not found: {employee_name}')
 
     def _get_service_id(self, service_name: str) -> int:
         """Gets id of service"""
@@ -89,7 +84,7 @@ class CRMRequest:
                 if service.name.casefold() == service_name.casefold():
                     return service.id
 
-        raise ValueError(f'Service {service_name} not found')
+        raise ValueError(f'Service not found: {service_name}')
 
     def get_categories(self) -> list[str]:
         """Returns all service categories as a list"""
@@ -103,52 +98,39 @@ class CRMRequest:
         """Returns service of a category or all services in default"""
         picked_services = []
 
-        if category_name is None:
-            for category in self.service_data:
-                for service in category.services:
-                    picked_services.append(service.name)
-
-            return picked_services
-
         for category in self.service_data:
-            if category.name == category_name:
+            if category_name is None or category.name == category_name:
                 for service in category.services:
                     picked_services.append(service.name)
 
-                return picked_services
+        if not picked_services:
+            raise ValueError(f'No services found in category: {category_name}')
+
+        return picked_services
 
     def get_employees(self, service_name: str = None) -> list[str]:
         """Return list of employees of service or all employees in default"""
         employees = []
-        if service_name is None:
-            for employee in self.employee_data:
-                full_name = ' '.join([employee.lastname, employee.firstname, employee.patronymic]).title()
-                employees.append(full_name)
+        service_id = None if service_name is None else self._get_service_id(service_name)
 
-            return employees
-
-        service_id = self._get_service_id(service_name)
         for employee in self.employee_data:
-            if service_id in employee.serviceEmployeesIds:
+            if service_name is None or service_id in employee.serviceEmployeesIds:
                 full_name = ' '.join([employee.lastname, employee.firstname, employee.patronymic]).title()
                 employees.append(full_name)
 
-                return employees
+        return employees
 
     def get_dates(self, employee_name: str) -> list[str]:
         """Returns list of free dates of an employee"""
         employee_id = self._get_employee_id(employee_name)
-
-        date_data: dict[str, dict[str, bool]] = r.get(url=self.dates_url).json()
-
-        free_dates = []
+        data = self._get_parsed_data(self.dates_url, Date)
         week = {0: 'ПН', 1: 'ВТ', 2: 'СР', 3: 'ЧТ', 4: 'ПТ', 5: 'СБ', 6: 'ВС'}
+        free_dates = []
 
-        for free_date, ids in date_data.items():
+        for free_date, ids in data.items():
             if employee_id in ids:
-                free_date = datetime.strptime(free_date, '%Y-%m-%d')
-                formated_date = week[free_date.weekday()] + free_date.strftime(' %d.%m')
-                free_dates.append(formated_date)
+                free_date = week[free_date.weekday()] + datetime.strftime(free_date, ' %d.%m')
+                free_dates.append(free_date)
 
         return free_dates
 
@@ -183,4 +165,4 @@ class CRMRequest:
 
 if __name__ == '__main__':
     req = CRMRequest()
-    print(req.service_data)
+    print(req.get_dates('Османов Ильяс Нариманович'))
