f4a8dba334a3dc70fee80dcce9835ff86c8d4ce8
Philipekum <filipp.tsekhansky@yandex.ru>
2023-08-17 22:39:58 +0300

    all text replaced with json

--- handlers/client_cmds.py
+++ handlers/client_cmds.py
@@ -2,16 +2,17 @@ from aiogram import Router, types
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import Command, Text
 from keyboards.client_kb import main_kb, info_kb, contact_kb
-import message_text
+from texts import json_reader
 from config import config
 
 router = Router()
+text = json_reader.read_json_file('texts/text_client_cmds.json')
 
 
 @router.message(Command('start'))
 async def cmd_start(message: types.Message, state: FSMContext):
     await state.clear()
-    await message.answer(text=message_text.start_text.format(message.from_user.full_name),
+    await message.answer(text=text['start_text'].format(message.from_user.full_name),
                          reply_markup=main_kb())
 
 
@@ -19,7 +20,7 @@ async def cmd_start(message: types.Message, state: FSMContext):
 @router.message(Text(startswith='Помощь', ignore_case=True))
 async def cmd_help(message: types.Message, state: FSMContext):
     await state.clear()
-    await message.answer(text=message_text.help_text,
+    await message.answer(text=text['help_text'],
                          reply_markup=main_kb())
 
 
@@ -27,20 +28,20 @@ async def cmd_help(message: types.Message, state: FSMContext):
 @router.message(Command('cancel'))
 async def cmd_back(message: types.Message, state: FSMContext):
     if await state.get_state() is None:
-        await message.answer(text=message_text.no_active_commands_to_cancel, reply_markup=main_kb())
+        await message.answer(text=text['no_active_commands_to_cancel'], reply_markup=main_kb())
 
     else:
         await state.clear()
-        await message.answer(text=message_text.commaned_canceled, reply_markup=main_kb())
+        await message.answer(text=text['commaned_canceled'], reply_markup=main_kb())
 
 
 @router.message(Text(startswith=('Инфо', 'О нас'), ignore_case=True))
 async def info_message(message: types.Message):
-    await message.answer(text=message_text.info,
+    await message.answer(text=text['info'],
                          reply_markup=info_kb())
 
 
 @router.message(Text(startswith='Администратор', ignore_case=True))
 async def admin_message(message: types.Message):
-    await message.answer(text=message_text.admin,
+    await message.answer(text=text['admin'],
                          reply_markup=contact_kb(config.admin_username.get_secret_value()))
